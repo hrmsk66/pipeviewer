b03641cd4e2e68bc3d520cfd61765754bc9a6af2
hrmsk66 <hrmsk66@gmail.com>
2020-03-22 19:58:19 +0900

    CH5-1 Creating a Library and Organizing it into Modules

---
+++ src/stats/timer.rs
@@ -0,0 +1,31 @@
+use std::time::{Duration, Instant};
+
+pub struct Timer {
+    pub last_timer: Instant,
+    pub delta: Duration,
+    pub period: Duration,
+    pub countdown: Duration,
+    pub ready: bool,
+}
+
+impl Timer {
+    pub fn new() -> Self {
+        let now = Instant::now();
+        Self {
+            last_timer: now,
+            delta: Duration::default(),
+            period: Duration::from_millis(100),
+            countdown: Duration::default(),
+            ready: true,
+        }
+    }
+    pub fn update(&mut self) {
+        let now = Instant::now();
+        self.delta = now - self.last_timer;
+        self.last_timer = now;
+        self.countdown = self.countdown.checked_sub(self.delta).unwrap_or_else(|| {
+            self.ready = true;
+            self.period
+        });
+    }
+}
